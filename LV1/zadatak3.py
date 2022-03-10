fff80f584698aa08e8b038f236b47adf36ec0171
Mihael Kolembus <mihaelkolembus@gmail.com>
2022-03-10 13:47:22 +0100

    Zadatak3

---
+++ LV1/zadatak3.py
@@ -0,0 +1,8 @@
+def ukupno(radni,placa):
+    print("Ukupna placa je :",radni*placa)
+
+radni_sati = float(input("Unesi radne sate: "))
+placa_po_satu = float(input("Placa po satu: "))
+ukupno(radni_sati,placa_po_satu)
+
+
